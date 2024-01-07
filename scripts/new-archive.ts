1b7704f61caa1d18ebd3d8358f22e45b0f818d7a
Super V 2038 <hello-super-v@outlook.com>
2024-01-07 14:11:00 +0800

    chore: 文章中英文间自动空格
    skip netlify

--- scripts/new-archive.ts
+++ scripts/new-archive.ts
@@ -1,7 +1,8 @@
 import { spawnSync } from "node:child_process"
 import { randomBytes } from "node:crypto"
-import { appendFileSync, writeFileSync } from "node:fs"
+import { writeFileSync } from "node:fs"
 import path from "node:path"
+import autocorrect from "autocorrect-node"
 import inquirer from "inquirer"
 import consola from "consola"
 import yaml from "yaml"
@@ -10,7 +11,7 @@ import { getDirname } from "./utils.js"
 const __dirname = getDirname(import.meta.url)
 
 const id = randomBytes(4).toString("hex").slice(0, 7) // 7 位随机十六进制
-consola.info("文章 ID：%d", id)
+consola.info("文章 ID：%s", id)
 const filePath = path.resolve(__dirname, `../src/archive/${id}.md`)
 
 const filter = (content: string) => {
@@ -19,25 +20,32 @@ const filter = (content: string) => {
   return content.split(",").map((item) => item.trim())
 }
 
-consola.info("多个值请用“,”分隔")
-const frontmatter = await inquirer.prompt([
+consola.info("以下字段可留空，多个值请用“,”分隔")
+const { title, ...frontmatter } = await inquirer.prompt([
   { type: "input", name: "category", message: "所属系列", filter },
   { type: "input", name: "tag", message: "文章标签", filter },
   { type: "input", name: "reference", message: "来源链接", filter },
-])
-const { title, content } = await inquirer.prompt([
   { type: "input", name: "title", message: "文章标题" },
+])
+let { content } = await inquirer.prompt<{ content: string }>([
   { type: "editor", name: "content", message: "输入文章内容" },
 ])
 
-writeFileSync(
-  filePath,
-  "---\n" + yaml.stringify(frontmatter) + "---\n\n# " + title + "\n\n",
-)
-appendFileSync(filePath, content)
-appendFileSync(filePath, "\n")
+content = autocorrect.format(`---
+${yaml.stringify(frontmatter)}
+---
+
+# ${title}
+
+${content}
+`) // 添加格式；中英文间加空格
+writeFileSync(filePath, content) // 写入文件
 consola.success("文章已保存")
 
-const check = spawnSync("pnpm", ["markdownlint-cli2", filePath, "--fix"])
+const check = spawnSync("pnpm", [ // markdownlint
+  "markdownlint-cli2",
+  filePath,
+  "--fix",
+])
 if (check.status) consola.info("已修复部分排版问题，剩余错误需手动修复")
 else consola.success("已修复排版问题")
--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,5 +1,11 @@
+import * as path from "node:path"
+import { fileURLToPath } from "node:url"
 import { simpleGit } from "simple-git"
 
 export const ARCHIVE_MESSAGE = "chore: 更新文章"
 
 export const git = simpleGit()
+
+export const getDirname = (url: string) => {
+  return path.dirname(fileURLToPath(url))
+}
